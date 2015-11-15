b4056f7924402a00521d182beb83113b10e40348
Miguel Benavente Valdes <benavente.miguel@gmail.com>
2015-11-15 15:49:52 -0600

    Initial Commit

---
+++ minireto2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  minireto2
+//
+//  Created by Miguel Benavente Valdés on 15/11/15.
+//  Copyright © 2015 Miguel Benavente Valdés. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
